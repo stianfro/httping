4ab34c4947e308a0e91ddfd5f3d1edfb4a837aae
Stian Frøystein <stianfroy@gmail.com>
2024-07-18 16:06:35 +0900

    feat: add support for get requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use reqwest::Error;
+use std::env::{self};
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("httping: missing URL");
+        eprintln!("Usage: httping [URL]");
+        std::process::exit(1);
+    }
+
+    if args.len() > 2 {
+        eprintln!("httping: can only use one URL");
+        eprintln!("Usage: httping [URL]");
+        std::process::exit(1);
+    }
+
+    let url = &args[1];
+
+    if Url::parse(url).is_err() {
+        eprintln!("Invalid URL: {}", url);
+        std::process::exit(1);
+    }
+
+    let now = Local::now();
+    let response = reqwest::get(url).await?;
+    let status_code = response.status();
+
+    println!("{} {} {}", now.to_rfc3339(), status_code, url);
+
+    Ok(())
 }
